37086a08c7b82980209c450b46e72bf0c0f06a3c
hilmoo <hilmusyaf2005@gmail.com>
2025-05-26 09:35:08 +0700

    add grade endpoint

---
+++ src/routes/api/data.grade.$examId.ts
@@ -0,0 +1,32 @@
+import { json } from "@tanstack/react-start";
+import { dbGetExamAttemptById } from "~/db/service/examAttempt";
+import { auth } from "~/lib/auth";
+
+export const ServerRoute = createServerFileRoute().methods({
+  GET: async ({ request, params }) => {
+    const { examId } = params;
+    const examIdNum = Number(examId);
+    if (isNaN(examIdNum)) {
+      return new Response(JSON.stringify({ error: "Invalid exam ID" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const headers = request.headers;
+    const session = await auth.api.getSession({
+      headers,
+      query: { disableCookieCache: true },
+    });
+    if (!session) {
+      return new Response(JSON.stringify({ error: "Unauthorized" }), {
+        status: 401,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const examResult = await dbGetExamAttemptById(examId, session.user.id);
+
+    return json(examResult);
+  },
+});
